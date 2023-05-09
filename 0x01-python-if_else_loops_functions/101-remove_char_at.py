d4a900018d4505ce1c7087a8717e63e53e9256ac
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-09 16:39:39 -0700

    101 commit 6

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,4 +4,6 @@ def remove_char_at(str, n):
     if n < 0 or n > len(str) - 1:
         return "{}".format(str, end='')
     else:
-        return "{}".format(''.join(str[i] for i in range(len(str)) if i != n), end='')
+        return "{}".format(''.join(
+            str[i] for i in range(len(str)) if i != n
+        ), end='')
