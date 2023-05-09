64b342f8fa78b9793332cc5b06d7dc279e423597
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-09 12:40:45 -0700

    2 commit 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 
-print("{}".format(''.join(chr(i) for i in range(97, 97 + 26))))
+#print("{}".format(''.join(chr(i) for i in range(97, 97 + 26))))
+print("{}".format('abcdefghijklmnopqrstuvwxyz'))
