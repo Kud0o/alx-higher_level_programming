a45c5dc74589af03a94657aced619dd6a1f1d37e
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-09 15:35:52 -0700

    12 commit 0

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(101):
+        if i % 3 == 0:
+            print("{d}".format(d="Fizz", end=''), end='')
+        if i % 5 == 0:
+            print("{d}".format(d="Buzz", end=''), end='')
+        if i % 3 != 0 and i % 5 != 0
+            print("{d}".format(d=i, end=''), end='')
+        print(" ")
