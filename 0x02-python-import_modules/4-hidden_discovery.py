672bc9ce494c0c0e6b7752d0efc25f63b8e9ebc5
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-11 09:45:55 -0700

    4 C 1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,11 +2,13 @@
 
 import hidden_4
 
+
 def print_module_names():
     for name in dir(hidden_4):
         if name[:2] == "__":
-            continue;
+            continue
         print(name)
+
 
 if __name__ == '__main__':
     print_module_names()
