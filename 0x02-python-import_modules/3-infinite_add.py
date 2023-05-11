5b4651af1880aca8e5188c871985dd6e893a966b
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-11 09:38:32 -0700

    3 C 1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import sys
 
-total = 0
-for i in range(1, len(sys.argv)):
-    total = total + int(sys.argv[i])
+if __name__ == '__main__':
+    import sys
 
-print(total)
+    total = 0
+    for i in range(1, len(sys.argv)):
+        total = total + int(sys.argv[i])
+
+    print(total)
