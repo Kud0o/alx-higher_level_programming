ad9175d0e79aa7d504774ffa0fe2b807a8e082e8
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-11 09:29:11 -0700

    0 C 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
-print("{a} + {b} = {sum}".format(a=a, b=b, sum=add(a, b)))
+if __name__ == '__main__':
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{a} + {b} = {sum}".format(a=a, b=b, sum=add(a, b)))
