46c8f671c36612f90b96a598ee74f93bca67662d
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-11 10:20:13 -0700

    100 C 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import *
 
+if __name__ == '__main__':
+    import sys
+    from calculator_1 import *
+    if len(sys.argv) != 4:
+        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        exit(1)
 
-if len(sys.argv) != 4:
-    print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
-    exit(1)
+    my_lib = {}
+    my_lib['+'] = add(int(sys.argv[1]), int(sys.argv[3]))
+    my_lib['-'] = sub(int(sys.argv[1]), int(sys.argv[3]))
+    my_lib[r"'*'"] = mul(int(sys.argv[1]), int(sys.argv[3]))
+    my_lib[r"\*"] = mul(int(sys.argv[1]), int(sys.argv[3]))
+    my_lib["*"] = mul(int(sys.argv[1]), int(sys.argv[3]))
+    my_lib['/'] = div(int(sys.argv[1]), int(sys.argv[3]))
 
-my_lib = {}
-my_lib['+'] = add(int(sys.argv[1]), int(sys.argv[3]))
-my_lib['-'] = sub(int(sys.argv[1]), int(sys.argv[3]))
-my_lib[r"'*'"] = mul(int(sys.argv[1]), int(sys.argv[3]))
-my_lib[r"\*"] = mul(int(sys.argv[1]), int(sys.argv[3]))
-my_lib["*"] = mul(int(sys.argv[1]), int(sys.argv[3]))
-my_lib['/'] = div(int(sys.argv[1]), int(sys.argv[3]))
-
-if sys.argv[2] not in my_lib.keys():
-    print("{}".format("Unknown operator. Available operators: +, -, * and /"))
-    exit(1)
-else:
-    print("{first} {operator} {second} = {result}".format(
-        first=sys.argv[1], second=sys.argv[3], operator=sys.argv[2],
-        result=my_lib[sys.argv[2]]))
+    if sys.argv[2] not in my_lib.keys():
+        print("{}"
+              .format("Unknown operator. Available operators: +, -, * and /")
+              )
+        exit(1)
+    else:
+        print("{first} {operator} {second} = {result}".format(
+            first=sys.argv[1], second=sys.argv[3], operator=sys.argv[2],
+            result=my_lib[sys.argv[2]]))
