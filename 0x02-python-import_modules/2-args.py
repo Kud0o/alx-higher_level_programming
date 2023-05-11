26d7a4b44ad84aa433d7d820db3287baa90dc90e
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-11 09:33:27 -0700

    2 C 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import sys
 
-if len(sys.argv) - 1 == 1:
-    print("{}".format("1 argument."))
-    print("{index}: {param}".format(index=1, param=sys.argv[1]))
-elif len(sys.argv) - 1 == 0:
-    print("{}".format("0 arguments."))
+if __name__ == '__main__':
+    import sys
 
-if len(sys.argv) - 1 > 1:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{index}: {param}".format(index=i, param=sys.argv[i]))
+    if len(sys.argv) - 1 == 1:
+        print("{}".format("1 argument."))
+        print("{index}: {param}".format(index=1, param=sys.argv[1]))
+    elif len(sys.argv) - 1 == 0:
+        print("{}".format("0 arguments."))
+
+    if len(sys.argv) - 1 > 1:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{index}: {param}".format(index=i, param=sys.argv[i]))
