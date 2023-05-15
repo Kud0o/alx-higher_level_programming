83658837fc6a78cdace949d60b144fcc4dbfc64f
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-15 14:00:52 -0700

    F 7 C 0

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < len(my_list) or idx > 0:
+        res = []
+        res += my_list[0:idx]
+        res += [element]
+        res += my_list[idx+1:]
+        return res
+    else:
+        return my_list[:]
+    
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    strl = list(my_string)
+    res = []
+    c = 0
+    for i in strl:
+        if i = 'c' and i = 'C':
+            continue
+        else:
+            res += i
+    return ''.join(res)
+    
