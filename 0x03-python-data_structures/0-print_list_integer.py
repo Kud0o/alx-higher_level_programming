3823029ccab0abb0f78e28348794b9d8b8cb083d
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-15 09:32:28 -0700

    F 3 commit 2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    print("{}".format(''.join(f"{i}\n" for i in my_list), end=''), end='')
+    if len(my_list) > 0:
+        print("{}".format(''.join(f"{i}\n" for i in my_list), end=''), end='')
