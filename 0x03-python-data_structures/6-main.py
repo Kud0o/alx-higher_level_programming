f5c95e603dc8168686ccb0924405de87fe0608fc
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-15 15:11:03 -0700

    6 C 7

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -10,4 +10,4 @@ matrix = [
 print_matrix_integer(matrix)
 print("--")
 print_matrix_integer()
-
+print_matrix_integer([])
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in matrix:
             if i:
-                print("{}".format(''.join("{:d}" for j in i[:-1]), end=' ') + "{}".format(f"{i[-1]}", end=''))
+                print("{}".format(''.join(f"{j:d} " for j in i[:-1]), end='') + "{}".format(f"{i[-1]}", end=''))
     else:
         print()
