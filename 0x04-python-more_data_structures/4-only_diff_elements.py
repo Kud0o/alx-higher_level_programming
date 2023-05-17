0e12921dcf6ae7f83c518c31dfed4c82aef6a807
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-17 16:36:27 -0700

    S x 1

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,10 +2,10 @@
 def only_diff_elements(set_1, set_2):
     res = set()
     for i in set_1:
-        if not i in set_2:
+        if i not in set_2:
             res.add(i)
 
     for i in set_2:
-        if not i in set_1:
+        if i not in set_1:
             res.add(i)
     return res
