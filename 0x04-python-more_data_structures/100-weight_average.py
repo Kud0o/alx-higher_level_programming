9e963e4e9e2b3617da10e6a9ba81b4345f09584c
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-17 18:02:16 -0700

    L 3 2

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list:
-        return sum(list(map(lambda x: x[0] * x[1], my_list))) /
-        sum(list(map(lambda x: x[1], my_list)))
+        i1 = sum(list(map(lambda x: x[0] * x[1], my_list)))
+        i2 = sum(list(map(lambda x: x[1], my_list)))
+        return i1 / i2
     else:
         return 0
