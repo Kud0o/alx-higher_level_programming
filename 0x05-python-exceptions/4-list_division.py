9a6630de328c9a496a6aa06b2f0a8f21cfa9f7e3
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-29 16:48:50 -0700

    4 commit 0

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res_list = []
+    for i in range(list_length):
+        try:
+            res_list.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            res_list.append(0)
+        except IndexError:
+            print("out of range")
+            res_list.append(0)
+        except ZeroDivisionError:
+            print("out of range")
+            res_list.append(0)
+        finally:
+            pass
+    return res_list
