56455c32a08fafac600e50c70fb624de0b04ff43
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-29 16:53:52 -0700

    4 commit 1

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             res_list.append(0)
         except ZeroDivisionError:
-            print("out of range")
+            print("division by 0")
             res_list.append(0)
         finally:
             pass
