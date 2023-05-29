95d6a262089b94c30d906fadbdcbed093fcdfb66
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-29 15:30:16 -0700

    0 C 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             c = c + 1
-    except:
+    except ErrorName:
         return c
-    
     else:
         return c
 
