ccb6840ad7402bdb2cc507589e1ca73580bd5394
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-29 15:09:14 -0700

    0 C 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            c = c + 1
+    except:
+        return c
+    
+    else:
+        return c
+
+    finally:
+        print()
