40f82fb1112c12655b982ee98f310ff666e29c3c
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-05-30 14:30:20 -0700

    F 3 3

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
+"""
+0-square.py: is an empty class Square that defines a square
+"""
+
 
 class Square:
+    """This is a empty class Square
+
+    the Class don't have args and Attributes
+
+    """
     pass
