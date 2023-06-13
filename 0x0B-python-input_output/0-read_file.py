389fc546ef3d692415f3bb82c0bb1418a55316f8
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-06-13 15:35:03 -0700

    io 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""Write a function that reads a text file (UTF8) and prints it to stdout."""
+
+
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')
-    except Exception:
-        pass
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
